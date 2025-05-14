3ea77a4a5c01db356215bcf845c98643ef0026a4
Sampinen <siiri.kalliomaki@helsinki.fi>
2025-05-14 21:36:15 +0300

    Jotain pohjaa

---
+++ dokumentaatio/ideointi.md
@@ -0,0 +1,5 @@
+
+
+Ideointia katsottu seuraavista lähteistä:
+
+- https://youtu.be/DV5d31z1xTI?si=vICaqkOYK8VR5vNz
---
+++ src/connect4.py
@@ -0,0 +1,6 @@
+
+
+class ConnectFour:
+    def __init__(self):
+        self.player1= ""
+        self.player2 = ""
