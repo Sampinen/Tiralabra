4a9e13b54e12268eeda35435bad64d581d15b4a4
Sampinen <siiri.kalliomaki@helsinki.fi>
2025-05-22 14:46:39 +0300

    tasks.py lisätty

---
+++ tasks.py
@@ -0,0 +1,32 @@
+from invoke import task
+from subprocess import call
+from sys import platform
+#pylint: skip-file
+
+# Taskit kopioitu Ohjelmistotekniikka kurssilla tekemästäni tedostosta
+@task
+def start(ctx):
+    ctx.run("python3 src/main.py", pty=True)
+
+@task
+def test(ctx):
+    """Runs coverage tests"""
+    ctx.run("coverage run --branch -m pytest", pty=True)
+
+
+@task
+def coverage_report(ctx):
+    """Returns coverage report"""
+    ctx.run("coverage html", pty=True)
+    if platform != "win32":
+        call(("xdg-open", "htmlcov/index.html"))
+
+@task
+def format(ctx):
+    """Redefines pylint format"""
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
+
+@task
+def lint(ctx):
+    """Checks the quality of the code"""
+    ctx.run("pylint src", pty=True)
