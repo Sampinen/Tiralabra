e02366b5b08b74e48dcc84d261560c789003cebb
Sampinen <siiri.kalliomaki@helsinki.fi>
2025-05-24 21:56:48 +0300

    score muuttuja, jonka ajatus on, että jälkeenpäin algoritmia voi kehittää suuntaan, joka osaa laskea monenko ruudun päässä ollaan voitosta

--- src/connect4.py
+++ src/connect4.py
@@ -36,14 +36,20 @@ class ConnectFour:
         if row > 7 or row < 1:
             print("Valitse numero väliltä 1 ja 7")
         if self.played[row] <6:
-            self.board[self.played[row]+1][row] = str(player)
+            column = self.played[row]+1
+            self.board[column][row] = str(player)
             self.played[row] +=1
-        self.print_board()
+            self.weights[self.played[row]][row] = 0
+            if column -1 in self.weights:
+                self.weights[column-1][row] += 2
 
-    def ask(self):
-        self.player1 = str(input("Pick a symbol: "))[0]+" "
+        self.print_board()
+            
+
+    def ask(self,player_name = None, pick_row = None):
+        self.player1 = str(input("Pick a symbol: "))[0]+" " if not player_name else player_name[0]
         while True:
-            row = int(input("Pick a row (1-7): "))
+            row = int(input("Pick a row (1-7): ")) if not pick_row else pick_row
             self.play(row, self.player1)
             win1 = self.check_win(self.player1)
             if win1:
@@ -56,53 +62,67 @@ class ConnectFour:
     def check_cell(self,c,r,p):
         """c=coulumn, r= row, p=player"""
         if self.board[c][r] == p:
-            return True
-        return False
+            return 1
+        if self.board[c][r] == "  ":
+            return 0
+        return -1
 
 
     def check_horizontal(self,c,r,p):
         """c=coulumn, r= row, p=player"""
+        score = 0
         for i in range(4):
-            if not self.check_cell(c,r+i,p):
-                return False
-        return True
+            cell = self.check_cell(c,r+i,p)
+            if cell == -1:
+                return -1
+            score += cell
+        return score
 
     def check_vertical(self,c,r,p):
         """c=coulumn, r= row, p=player"""
+        score = 0
         for i in range(4):
-            if not self.check_cell(c+i,r,p):
-                return False
-        return True
+            cell = self.check_cell(c+i,r,p)
+            if cell == -1:
+                return -1
+            score += cell
+        return score
 
     def check_diagonal_up(self,c,r,p):
         """c=coulumn, r= row, p=player"""
+        score = 0
         for i in range(4):
-            if not self.check_cell(c+i,r+i,p):
-                return False
-        return True
+            cell = self.check_cell(c+i,r+i,p)
+            if cell == -1:
+                return -1
+            score += cell
+        return score
 
     def check_diagonal_down(self,c,r,p):
         """c=coulumn, r= row, p=player"""
+        score = 0
         for i in range(4):
-            if not self.check_cell(c-i,r+i,p):
-                return False
-        return True
+            cell = self.check_cell(c-i,r+i,p)
+            if cell == -1:
+                return -1
+            score += cell
+        return score
 
     def check_win(self,player):
         for c in range(1,7):
             for r in range(1,5):
-                if self.check_horizontal(c,r,player):
+                if self.check_horizontal(c,r,player) ==4:
                     print(player + " Won!")
                     return True
                 if c < 4:
-                    if self.check_vertical(c,r,player):
+                    if self.check_vertical(c,r,player) ==4:
                         print(player + " Won!")
                         return True
-                    if self.check_diagonal_up(c,r,player):
+                    if self.check_diagonal_up(c,r,player)==4:
                         print(player + " Won!")
                         return True
                 if c > 3:
-                    if self.check_diagonal_down(c,r,player):
+                    if self.check_diagonal_down(c,r,player)==4:
                         print(player + " Won!")
                         return True
         return False
