6820c847069f1a17a72ee0f26320f4d426501790
Sampinen <siiri.kalliomaki@helsinki.fi>
2025-06-19 16:51:10 +0300

    Nyt minmax vaikuttaa toimivan paremmin

--- src/connect4.py
+++ src/connect4.py
@@ -47,13 +47,17 @@ class ConnectFour:
             win1 = self.check_win_cell(row,column,self.player1,self.board)
             self.print_board()
             if win1:
+                print("Player win")
                 return False
-            best_column = self.minmax(self.board,self.played,3,False)[0]
+            minmax = self.minmax(self.board,self.played,9,False)
+            best_column = minmax[0]
+            print(minmax)
             row2 = self.played[best_column]+1
             self.board = self.play(best_column,self.player2,self.board,self.played)
-            win2 = self.check_win_cell(best_column,row2,self.player2,self.board)
+            win2 = self.check_win_cell(row2,best_column,self.player2,self.board)
             self.print_board()
             if win2:
+                print("AI win")
                 return False
 
     def is_valid_location(self,r,c):
@@ -68,18 +72,19 @@ class ConnectFour:
                     score +=1
                     i += 1
                 else:
-                    i = -1
+                    break
             else:
-                i = -1
+                break
+        i = -1
         while i < 0:
             if self.is_valid_location(r,c+i):
                 if board[r][c+i] == p:
                     score += 1
                     i -= 1
                 else:
-                    i = 0
+                    break
             else:
-                i = 0
+                break
         return score >=4
 
 
@@ -92,9 +97,9 @@ class ConnectFour:
                     score += 1
                     i -= 1
                 else:
-                    i = 9999
+                    break
             else:
-                i = 9999
+                break
         return score >=4
 
     def check_win_diagonal_down(self,r,c,p,board):
@@ -106,18 +111,19 @@ class ConnectFour:
                     score +=1
                     i += 1
                 else:
-                    i = -1
+                    break
             else:
-                i = -1
+                break
+        i = -1
         while i < 0:
             if self.is_valid_location(r-i,c+i):
                 if board[r-i][c+i] == p:
                     score += 1
                     i -= 1
                 else:
-                    i = 9999
+                    break
             else:
-                i = 9999
+                break
         return score >=4
 
 
@@ -130,18 +136,19 @@ class ConnectFour:
                     score +=1
                     i += 1
                 else:
-                    i = -1
+                    break
             else:
-                i = -1
+                break
+        i = -1
         while i < 0:
             if self.is_valid_location(r+i,c+i):
                 if board[r+i][c+i] == p:
                     score += 1
                     i -= 1
                 else:
-                    i = 0
+                    break
             else:
-                i = 0
+                break
         return score >=4
 
     def check_win_cell(self,r,c,p,board):
@@ -151,9 +158,9 @@ class ConnectFour:
         score4 = self.check_win_vertical(r,c,p,board)
         return score1 or score2 or score3 or score4
 
-    def minmax(self,board,played,depth,maxplayer,alpha=-99999999,beta=9999999):
+    def minmax(self,board,played,depth,maxplayer,alpha=-99999999,beta=9999999,columnorder=[4,3,5,2,6,1,7]):
         if depth == 0:
-            return None,0
+            return columnorder[0],0
         column = 4
         if maxplayer:
             value = -99999999
@@ -167,7 +174,7 @@ class ConnectFour:
                     self.play(c,self.player1,newboard,newplayed)
                     if self.check_win_cell(r,c,self.player1,newboard):
                         #print("-"*(3 - depth), "Player win found!")
-                        return column, 9999999
+                        return c, 9999999
                     #print("-"*(3 - depth), "Searching", c)
                     new_value = self.minmax(newboard,newplayed,depth-1,False,alpha,beta)[1]
                     if new_value > value:
@@ -180,7 +187,7 @@ class ConnectFour:
             return column, value
         else: #minplayer, AI
             value = 9999999
-            for c in range(1,self.columncount+1):
+            for c in self.roworder:
                 if played[c] >= self.columncount:
                     pass
                 else:
@@ -190,16 +197,16 @@ class ConnectFour:
                     self.play(c,self.player2,newboard,newplayed)
 
                     if self.check_win_cell(r,c,self.player2,newboard):
-                        print(" "*(3 - depth), "AI win found")
-                        return column, -9999999
-                    print(" "*(3 - depth), "Searching", c)
+                        #print(" "*(3 - depth), "AI win found")
+                        return c, -9999999
+                    #print(" "*(3 - depth), "Searching", c)
                     new_value = self.minmax(newboard,newplayed,depth-1,True,alpha,beta)[1]
                     if new_value < value:
                         value = new_value
                         column = c
                     beta = min(beta,value)
-                    if beta <= alpha:
+                    if alpha >= beta:
                         break
 
-            print(" "*(3 - depth), value, column)
+            #print(" "*(3 - depth), value, column)
             return column, value
